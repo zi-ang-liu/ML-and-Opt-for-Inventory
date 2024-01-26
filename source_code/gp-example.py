cd08f56f72bdca04043b6487857c4b4399a28439
Ziang LIU <50853393+zi-ang-liu@users.noreply.github.com>
2024-01-26 15:43:48 +0900

    a

--- source_code/gp-example.py
+++ source_code/gp-example.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 def f(x):
     return x * np.sin(x)
 
-X = np.linspace(0, 20, 200)
+X = np.linspace(0, 10, 200)
 y = f(X)
 
 
@@ -20,11 +20,16 @@ gp.fit(X_train.reshape(-1, 1), y_train)
 
 mean, std = gp.predict(X.reshape(-1, 1), return_std=True)
 
+# aquisition function
+lb = mean - 10 * std
+
+
 # plot the function, the prediction and the 95% confidence interval
 plt.plot(X, y, label='True function: $f(x) = x\,\sin(x)$')
 plt.plot(X, mean, 'k--', label='GP mean')
 plt.fill_between(X, mean - 1.96 * std, mean + 1.96 * std, alpha=0.2, label=r'95% confidence interval')
 plt.scatter(X_train, y_train, label='Training data')
+plt.plot(X, lb, 'r--', label='Lower Confidence Bound')
 plt.legend()
 plt.savefig('gp-example.pdf')
 
